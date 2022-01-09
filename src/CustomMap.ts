38832e9bd7c458bb0f204ec5776c40cbaf214957
Olusola Oseni <kenolusola@gmail.com>
2022-01-09 17:19:36 +0100

    add more typing with to user and company class

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,6 @@
 // instructions to every other class
 // on how how they can be an argument to 'addMarker'
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
